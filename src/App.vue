a0f19265ed49b564a45c597b1c0c11bedc202e73
Seth Wright <asethwright@gmail.com>
2017-02-07 19:05:26 -0500

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <Player v-for="player in players" :player="player"></Player>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Player from './components/Player'
+
+export default {
+  name: 'app',
+
+  components: {
+    Player
+  },
+
+  data () {
+    return {
+      players: []
+    }
+  },
+
+  mounted () {
+    axios.get('/static/basketball.json')
+      .then((response) => {
+        console.log(response.data)
+        this.players = response.data
+      })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Player.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="Player">
+    <h3>{{ player.first }} {{ player.last }}</h3>
+    <p>{{ shootingPct }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'player'
+  ],
+
+  computed: {
+    shootingPct () {
+      return this.player.fieldGoals / this.player.fieldGoalsAttempted
+    }
+  }
+}
+</script>
+
+<style scoped>
+.Player {
+}
+</style>
